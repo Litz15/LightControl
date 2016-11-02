470d170c1c65bb9eb4cbd13ccd10554dd362b424
Fabio Cortes <fcor14@hotmail.com>
2016-11-02 15:01:34 -0500

    5 niveles de luz añadidos T=500ms Saltos de 90Lux en el rango 350-700

--- Litz_1.0.ino
+++ Litz_1.0.ino
@@ -6,57 +6,41 @@ SFE_TSL2561 light; // Create an SFE_TSL2561 object, here called "light":
 
 // Global variables:
 
-boolean gain;     // Gain setting, 0 = X1, 1 = X16;
-unsigned int ms;  // Integration ("shutter") time in milliseconds
-double lux;    // Resulting lux value
-boolean good;  // True if neither sensor is saturated
+boolean gain;       // Gain setting, 0 = X1, 1 = X16;
+unsigned int ms;    // Integration ("shutter") time in milliseconds
+double lux;         // Resulting lux value
+boolean good;       // True if neither sensor is saturated
 const int nivel1=4; //Nivel 1 
+const int nivel2=5; //Nivel 2
+const int nivel3=6; //Nivel 3
+const int nivel4=7; //Nivel 4
+const int nivel5=8; //Nivel 5
 
 void setup()
 {
   Serial.begin(9600); // Initialize the Serial port:
   pinMode(nivel1,OUTPUT); //Set output for Nivel 1
+  pinMode(nivel2,OUTPUT); //Set output for Nivel 2
+  pinMode(nivel3,OUTPUT); //Set output for Nivel 3
+  pinMode(nivel4,OUTPUT); //Set output for Nivel 4
+  pinMode(nivel5,OUTPUT); //Set output for Nivel 5
  
   light.begin();  // Initialize the SFE_TSL2561 library
 
-
-  // The light sensor has a default integration time of 402ms,
-  // and a default gain of low (1X).
-  
-  // If you would like to change either of these, you can
-  // do so using the setTiming() command.
-  
-  // If gain = false (0), device is set to low gain (1X)
-  // If gain = high (1), device is set to high gain (16X)
-
-  gain = 0;
-
-  // If time = 0, integration will be 13.7ms
-  // If time = 1, integration will be 101ms
-  // If time = 2, integration will be 402ms
-  // If time = 3, use manual start / stop to perform your own integration
-
-  unsigned char time = 2;
-
-  // setTiming() will set the third parameter (ms) to the
-  // requested integration time in ms (this will be useful later):
-  
+  gain = 0;   // If gain = false (0), device is set to low gain (1X)
+ 
+  unsigned char time = 2; // If time = 2, integration will be 402ms
+ 
   light.setTiming(gain,time,ms);
+  
+  light.setPowerUp(); // To start taking measurements, power up the sensor:
 
-  // To start taking measurements, power up the sensor:
-  
-  Serial.println("Powerup...");
-  light.setPowerUp();
-  
-  // The sensor will now gather light during the integration time.
-  // After the specified time, you can retrieve the result from the sensor.
-  // Once a measurement occurs, another integration period will start.
 }
 
 void loop()
 {
  
-  delay(1000);
+  delay(500);
  
   // Retrieve the data from the device:
 
@@ -64,30 +48,66 @@ void loop()
   
   if (light.getData(data0,data1))
   {
-      
-    // Perform lux calculation:
-    good = light.getLux(gain,ms,data0,data1,lux);
-    
-    // Print out the results:
-    Serial.println(lux);
- 
+    good = light.getLux(gain,ms,data0,data1,lux); // Perform lux calculation:
   }
   else
   {
-    // getData() returned false because of an I2C error, inform the user.
-
-    byte error = light.getError();
+    byte error = light.getError(); // getData() returned false because of an I2C error, inform the user.
     printError(error);
   }
 
-  if(lux<100){
+  
+    if(lux<300){
     digitalWrite(nivel1,HIGH);
-  }
-  else{
+    digitalWrite(nivel2,LOW);
+    digitalWrite(nivel3,LOW);
+    digitalWrite(nivel4,LOW);
+    digitalWrite(nivel5,LOW);
+    }
+    if(lux>300&&lux<390){
     digitalWrite(nivel1,LOW);
+    digitalWrite(nivel2,HIGH);
+    digitalWrite(nivel3,LOW);
+    digitalWrite(nivel4,LOW);
+    digitalWrite(nivel5,LOW);
+    }
+    if(lux>390&&lux<480){
+    digitalWrite(nivel1,LOW);
+    digitalWrite(nivel2,LOW);
+    digitalWrite(nivel3,HIGH);
+    digitalWrite(nivel4,LOW);
+    digitalWrite(nivel5,LOW);
+    }
+    if(lux>570&&lux<660){
+    digitalWrite(nivel1,LOW);
+    digitalWrite(nivel2,LOW);
+    digitalWrite(nivel3,LOW);
+    digitalWrite(nivel4,HIGH);
+    digitalWrite(nivel5,LOW);
+    }
+    if(lux>660&&lux<750){
+    digitalWrite(nivel1,LOW);
+    digitalWrite(nivel2,LOW);
+    digitalWrite(nivel3,LOW);
+    digitalWrite(nivel4,LOW);
+    digitalWrite(nivel5,HIGH);
+    }
+  if(lux>750){
+    digitalWrite(nivel1,LOW);
+    digitalWrite(nivel2,LOW);
+    digitalWrite(nivel3,LOW);
+    digitalWrite(nivel4,LOW);
+    digitalWrite(nivel5,LOW);
     }
   
 }
+
+
+
+
+
+
+
 
 void printError(byte error)
   // If there's an I2C error, this function will
