9c3707590908987533d9513ae3d161f0fecd6c03
Fabio Cortes <fcor14@hotmail.com>
2016-11-02 14:37:54 -0500

    Se limpió basura del código - Pruena on/off con 100 lux y 1s

--- Litz_1.0.ino
+++ Litz_1.0.ino
@@ -1,103 +1,24 @@
-/* SparkFun TSL2561 library example sketch
-
-This sketch shows how to use the SparkFunTSL2561
-library to read the AMS/TAOS TSL2561
-light sensor.
-
-Product page: https://www.sparkfun.com/products/11824
-Hook-up guide: https://learn.sparkfun.com/tutorials/getting-started-with-the-tsl2561-luminosity-sensor
-
-Hardware connections:
-
-3V3 to 3.3V
-GND to GND
-
-(WARNING: do not connect 3V3 to 5V
-or the sensor will be damaged!)
-
-You will also need to connect the I2C pins (SCL and SDA) to your Arduino.
-The pins are different on different Arduinos:
-
-                    SDA    SCL
-Any Arduino         "SDA"  "SCL"
-Uno, Redboard, Pro  A4     A5
-Mega2560, Due       20     21
-Leonardo            2      3
-
-You do not need to connect the INT (interrupt) pin
-for basic operation.
-
-Operation:
-
-Upload this sketch to your Arduino, and open the
-Serial Monitor window to 9600 baud.
-
-Have fun! -Your friends at SparkFun.
-
-Our example code uses the "beerware" license.
-You can do anything you like with this code.
-No really, anything. If you find it useful,
-buy me a beer someday.
-
-V10 Mike Grusin, SparkFun Electronics 12/26/2013
-Updated to Arduino 1.6.4 5/2015
-*/
-
-// Your sketch must #include this library, and the Wire library
-// (Wire is a standard library included with Arduino):
 
 #include <SparkFunTSL2561.h>
 #include <Wire.h>
 
-// Create an SFE_TSL2561 object, here called "light":
-
-SFE_TSL2561 light;
+SFE_TSL2561 light; // Create an SFE_TSL2561 object, here called "light":
 
 // Global variables:
 
 boolean gain;     // Gain setting, 0 = X1, 1 = X16;
 unsigned int ms;  // Integration ("shutter") time in milliseconds
+double lux;    // Resulting lux value
+boolean good;  // True if neither sensor is saturated
+const int nivel1=4; //Nivel 1 
 
 void setup()
 {
-  // Initialize the Serial port:
-  
-  Serial.begin(9600);
-  Serial.println("TSL2561 example sketch");
+  Serial.begin(9600); // Initialize the Serial port:
+  pinMode(nivel1,OUTPUT); //Set output for Nivel 1
+ 
+  light.begin();  // Initialize the SFE_TSL2561 library
 
-  // Initialize the SFE_TSL2561 library
-
-  // You can pass nothing to light.begin() for the default I2C address (0x39),
-  // or use one of the following presets if you have changed
-  // the ADDR jumper on the board:
-  
-  // TSL2561_ADDR_0 address with '0' shorted on board (0x29)
-  // TSL2561_ADDR   default address (0x39)
-  // TSL2561_ADDR_1 address with '1' shorted on board (0x49)
-
-  // For more information see the hookup guide at: https://learn.sparkfun.com/tutorials/getting-started-with-the-tsl2561-luminosity-sensor
-
-  light.begin();
-
-  // Get factory ID from sensor:
-  // (Just for fun, you don't need to do this to operate the sensor)
-
-  unsigned char ID;
-  
-  if (light.getID(ID))
-  {
-    Serial.print("Got factory ID: 0X");
-    Serial.print(ID,HEX);
-    Serial.println(", should be 0X5X");
-  }
-  // Most library commands will return true if communications was successful,
-  // and false if there was a problem. You can ignore this returned value,
-  // or check whether a command worked correctly and retrieve an error code:
-  else
-  {
-    byte error = light.getError();
-    printError(error);
-  }
 
   // The light sensor has a default integration time of 402ms,
   // and a default gain of low (1X).
@@ -120,7 +41,6 @@ void setup()
   // setTiming() will set the third parameter (ms) to the
   // requested integration time in ms (this will be useful later):
   
-  Serial.println("Set timing...");
   light.setTiming(gain,time,ms);
 
   // To start taking measurements, power up the sensor:
@@ -135,60 +55,22 @@ void setup()
 
 void loop()
 {
-  // Wait between measurements before retrieving the result
-  // (You can also configure the sensor to issue an interrupt
-  // when measurements are complete)
-  
-  // This sketch uses the TSL2561's built-in integration timer.
-  // You can also perform your own manual integration timing
-  // by setting "time" to 3 (manual) in setTiming(),
-  // then performing a manualStart() and a manualStop() as in the below
-  // commented statements:
-  
-  // ms = 1000;
-  // light.manualStart();
-  delay(ms);
-  // light.manualStop();
-  
-  // Once integration is complete, we'll retrieve the data.
-  
-  // There are two light sensors on the device, one for visible light
-  // and one for infrared. Both sensors are needed for lux calculations.
-  
+ 
+  delay(1000);
+ 
   // Retrieve the data from the device:
 
   unsigned int data0, data1;
   
   if (light.getData(data0,data1))
   {
-    // getData() returned true, communication was successful
-    
-    Serial.print("data0: ");
-    Serial.print(data0);
-    Serial.print(" data1: ");
-    Serial.print(data1);
-  
-    // To calculate lux, pass all your settings and readings
-    // to the getLux() function.
-    
-    // The getLux() function will return 1 if the calculation
-    // was successful, or 0 if one or both of the sensors was
-    // saturated (too much light). If this happens, you can
-    // reduce the integration time and/or gain.
-    // For more information see the hookup guide at: https://learn.sparkfun.com/tutorials/getting-started-with-the-tsl2561-luminosity-sensor
-  
-    double lux;    // Resulting lux value
-    boolean good;  // True if neither sensor is saturated
-    
+      
     // Perform lux calculation:
-
     good = light.getLux(gain,ms,data0,data1,lux);
     
     // Print out the results:
-  
-    Serial.print(" lux: ");
-    Serial.print(lux);
-    if (good) Serial.println(" (good)"); else Serial.println(" (BAD)");
+    Serial.println(lux);
+ 
   }
   else
   {
@@ -197,6 +79,14 @@ void loop()
     byte error = light.getError();
     printError(error);
   }
+
+  if(lux<100){
+    digitalWrite(nivel1,HIGH);
+  }
+  else{
+    digitalWrite(nivel1,LOW);
+    }
+  
 }
 
 void printError(byte error)
@@ -228,4 +118,5 @@ void printError(byte error)
       Serial.println("unknown error");
   }
 }
+
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
